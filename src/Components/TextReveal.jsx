d9f846c33788e9eda0934b7362ca665087c4bd6e
realsamiul <realsamiulkarim@gmail.com>
2025-09-18 08:18:27 +0600

    5

--- src/Components/TextReveal.jsx
+++ src/Components/TextReveal.jsx
@@ -1,20 +1,24 @@
+// src/Components/TextReveal.jsx
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const TextReveal = ({ children, className }) => {
   const el = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
+    // This GSAP animation will be triggered when the element scrolls into view
     gsap.from(el.current, {
+      scrollTrigger: {
+        trigger: el.current,
+        start: 'top 90%', // Starts animation when 90% of the element is visible
+        toggleActions: 'play none none none',
+      },
       y: '110%',
       rotate: 10,
       duration: 1.2,
       ease: 'power4.out',
-      scrollTrigger: {
-        trigger: el.current,
-        start: 'top 95%',
-        toggleActions: 'play none none none',
-      },
     });
   }, []);
 
